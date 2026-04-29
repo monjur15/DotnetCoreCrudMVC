be2439bf926797731178105c908639a3ad20a983
Monjur <monjur.morshed@azolutionse.com>
2026-04-29 23:25:17 +0600

    Data model

---
+++ DotnetCoreCrudMVC/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotnetCoreCrudMVC.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display(Name ="Student ID")]
+        public int StudentID { get; set; }
+        [Required]
+        [Display(Name ="Student Name")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Course { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
